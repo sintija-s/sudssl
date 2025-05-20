0dedf46a645167d7305dd4c3977dc5cdfb4e77de
sintija-s <sintija.stevanoska@ijs.si>
2025-05-20 16:51:34 +0200

    change tassel to main

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ random_seeds_l = [2, 8, 32, 64, 128, 256, 512, 1024, 2048, 4096]
 # Configurable list of labeled data amounts for semi-supervised learning
 number_of_labeled_examples_l = [50, 100, 200, 350, 500]
 
-# All experimental settings for running tassel.py are below:
+# All experimental settings for running main.py are below:
 MODELS=['tabnet', 'scarf']
 DATASETS=[3, 6, 28, 32, 44, 46, 151, 182, 300, 1590, 4134, 1489, 1497, 1486, 1475, 4534, 1461, 4538, 1478, 40499, 40668, 40983, 41027, 23517, 40923, 40978, 40670, 40701]
 SEEDS=[2, 8, 32, 64, 128, 256, 512, 1024, 2048, 4096]
