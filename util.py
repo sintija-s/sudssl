9494e1e59ce4a17de17a6aecbc863432b59623ff
sintija-s <sintija.stevanoska@ijs.si>
2025-05-30 22:10:21 +0200

    add results for both models

--- util.py
+++ util.py
@@ -7,6 +7,33 @@ import torch
 from sklearn import set_config
 
 import glob
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+
+plt.rcParams.update({
+    "font.family": "serif",
+    "font.serif": ['DejaVu Serif', 'Bitstream Vera Serif', 'Computer Modern Roman', 'New Century Schoolbook', 'Century Schoolbook L', 'Utopia', 'ITC Bookman', 'Bookman', 'Nimbus Roman No9 L', 'Palatino', 'Charter', 'serif'],
+    "axes.titlesize": 14,
+    "axes.labelsize": 12,
+    "legend.title_fontsize": 12,
+    "legend.fontsize": 11,
+    "xtick.labelsize": 11,
+    "ytick.labelsize": 11,
+    "lines.linewidth": 2,
+    "lines.markersize": 6,
+    "legend.frameon": False,
+    "figure.figsize": (9, 3)  # standard size for subplots
+})
+COLORS = ['#4c90b8', '#2ac3c1', '#f5b811', '#de653e', '#ff912a']
+color_dict = {
+    'BaselineAll': 'black',
+    'BaselineNone': 'gray',
+    'DisagreementSampling(DT)': COLORS[3],
+    'DisagreementSampling(DS)': COLORS[4],
+    'RandomSampling': COLORS[0],
+    # add other sampling methods as needed
+}
 
 
 # reads all csv files in one folder
@@ -61,46 +88,54 @@ def get_all_linear_evaluation_results(base_results_folder):
         "f1_score_test",
     ]
 
-    for dataset_folder in os.listdir(base_results_folder):
-        dataset_path = os.path.join(base_results_folder, dataset_folder)
-        if os.path.isdir(dataset_path):
-            pretraining_eval_path = os.path.join(dataset_path, "pretraining_evaluation")
-            if os.path.isdir(pretraining_eval_path):
-                # Find all CSV files matching the pattern
-                search_pattern = os.path.join(
-                    pretraining_eval_path, "linear_probe_results_*.csv"
-                )
-                for csv_file_path in glob.glob(search_pattern):
-                    try:
-                        df_current_file = pd.read_csv(csv_file_path)
-                        model = csv_file_path.rsplit("_", 1)[-1].rsplit(".", 1)[0]
-                        df_current_file["model"] = model
-                        # Filter out rows with "_validation" in the metric column
-                        df_current_filtered = df_current_file[
-                            ~df_current_file["metric"].str.contains("_validation")
-                        ]
-                        # Create clustering results table for the current file
-                        current_clustering = df_current_filtered[
-                            df_current_filtered["metric"].isin(clustering_metrics)
-                        ]
-                        all_clustering_dfs.append(current_clustering)
-                        # Create linear probing results table for the current file
-                        current_linear_probing = df_current_filtered[
-                            df_current_filtered["metric"].isin(linear_probing_metrics)
-                        ]
-                        all_linear_probing_dfs.append(current_linear_probing)
-                    except Exception as e:
-                        print(f"Error processing file {csv_file_path}: {e}")
+    # for dataset_folder in os.listdir(base_results_folder):
+    #     dataset_path = os.path.join(base_results_folder, dataset_folder)
+    #     if os.path.isdir(dataset_path):
+    #         pretraining_eval_path = os.path.join(dataset_path, "pretraining_evaluation")
+    #         if os.path.isdir(pretraining_eval_path):
+    #             # Find all CSV files matching the pattern
+    #             search_pattern = os.path.join(
+    #                 pretraining_eval_path, "linear_probe_results_*.csv"
+    #             )
+    #             for csv_file_path in glob.glob(search_pattern):
+    #                 try:
+    #                     df_current_file = pd.read_csv(csv_file_path)
+    #                     model = csv_file_path.rsplit("_", 1)[-1].rsplit(".", 1)[0]
+    #                     df_current_file["model"] = model
+    #                     # Filter out rows with "_validation" in the metric column
+    #                     df_current_filtered = df_current_file[
+    #                         ~df_current_file["metric"].str.contains("_validation")
+    #                     ]
+    #                     # Create clustering results table for the current file
+    #                     current_clustering = df_current_filtered[
+    #                         df_current_filtered["metric"].isin(clustering_metrics)
+    #                     ]
+    #                     all_clustering_dfs.append(current_clustering)
+    #                     # Create linear probing results table for the current file
+    #                     current_linear_probing = df_current_filtered[
+    #                         df_current_filtered["metric"].isin(linear_probing_metrics)
+    #                     ]
+    #                     all_linear_probing_dfs.append(current_linear_probing)
+    #                 except Exception as e:
+    #                     print(f"Error processing file {csv_file_path}: {e}")
+    #
+    # final_clustering_df = pd.DataFrame()
+    # if all_clustering_dfs:
+    #     final_clustering_df = pd.concat(all_clustering_dfs, ignore_index=True)
+    #
+    # final_linear_probing_df = pd.DataFrame()
+    # if all_linear_probing_dfs:
+    #     final_linear_probing_df = pd.concat(all_linear_probing_dfs, ignore_index=True)
 
-    final_clustering_df = pd.DataFrame()
-    if all_clustering_dfs:
-        final_clustering_df = pd.concat(all_clustering_dfs, ignore_index=True)
+    dfs = []
+    for file_path in glob.glob(f'{base_results_folder}/results_embedding_*.csv'):
+        dfs.append(pd.read_csv(file_path))
+    df_all = pd.concat(dfs, ignore_index=True)
 
-    final_linear_probing_df = pd.DataFrame()
-    if all_linear_probing_dfs:
-        final_linear_probing_df = pd.concat(all_linear_probing_dfs, ignore_index=True)
+    clustering_df = df_all[df_all["metric"].isin(clustering_metrics)]
+    linear_probing_df = df_all[df_all["metric"].isin(linear_probing_metrics)]
 
-    return final_clustering_df, final_linear_probing_df
+    return clustering_df, linear_probing_df
 
 
 def clean_linear_probing_results(final_clustering_df, final_linear_probing_df):
@@ -196,6 +231,19 @@ def get_best_performing_per_metric(df, aggregation_group, metric="auprc_macro_te
 
     return df_best_bracket
 
+def make_baselines_for_q1(use_model, df):
+    if use_model == "both":
+        q1_df = df.copy()
+    else:
+        q1_df = df.loc[df['model'] == use_model].copy()
+
+    # calculate percentages
+    q1_df['of_baseline_all'] = (q1_df['value'] / q1_df['baseline_all']) * 100
+    q1_df['of_baseline_none'] = (q1_df['value'] / q1_df['baseline_none']) * 100
+    q1_df['of_baseline_raw'] = (q1_df['value'] / q1_df['baseline_raw_features']) * 100
+    q1_df['none_of_all'] = (q1_df['baseline_none'] / q1_df['baseline_all']) * 100
+
+    return q1_df
 
 def get_pivot_table_for_q1(df, value_columns, aggregation='mean'):
     df_prepivot = df.copy()
@@ -221,3 +269,37 @@ def get_pivot_table_for_q1(df, value_columns, aggregation='mean'):
     pivot_summary.rename(columns={"dataset": "Total Datasets"}, inplace=True)
 
     return pivot_summary
+
+def get_plot_q1(use_model, df):
+    if use_model == "both":
+        df_tmp = df.copy()
+    else:
+        df_tmp = df.loc[df['model'] == use_model].copy()
+
+    fig, ax = plt.subplots(figsize=(6, 2))  # Set width and height in inches
+
+    g = sns.lineplot(
+        data=df_tmp,
+        x='n_sample',
+        y='% of BaselineAll',
+        hue='sampling_method',
+        palette=color_dict,
+        errorbar=None,
+        ax=ax
+    )
+
+    ax.set_xlabel('n_sample')
+    ax.set_ylabel('AUPRC\n% of BaselineAll')
+
+    # Move legend to the right outside the plot
+    legend = ax.legend(
+        title='Sampling Method',
+        bbox_to_anchor=(1.1, 0.5),
+        loc='center left',
+        borderaxespad=0,
+        frameon=False
+    )
+
+    plt.tight_layout()
+    # plt.savefig('plots/1_2_nsample_embedding_space.pdf', bbox_inches='tight')
+    plt.show()
